517a985d639cf540d47b6f2d925e47ff017091c9
Ralph Schmieder <rschmied@cisco.com>
2016-11-24 15:48:03 +0100

    deleted some unnecessary files
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- DOCKER/env.sh
+++
@@ -1,6 +0,0 @@
-export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://172.16.20.128:2376"
-export DOCKER_CERT_PATH="/Users/rschmied/.docker/machine/machines/docker-new2"
-export DOCKER_MACHINE_NAME="docker-new2"
-# Run this command to configure your shell:
-# eval $(docker-machine env docker-new2)
